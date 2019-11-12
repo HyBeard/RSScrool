939b4eacc890699eb1491bd406b9f4448902c83c
HyBeard <Kaptsevich.i@gmail.com>
2019-11-13 00:40:52 +0300

    feat: selectTool + changeColor functions into View

--- palette/src/js/components/View.js
+++ palette/src/js/components/View.js
@@ -20,4 +20,33 @@ export default class View {
     return newElement;
   }
 
+  selectTool(tool = this.currentTool) {
+    const selectedTool = tool;
+
+    if (this.currentTool) this.currentTool.classList.remove('active');
+
+    selectedTool.classList.add('active');
+    this.currentTool = selectedTool;
+  }
+
+  updateLastColors(state) {
+    const { primColor } = state.general;
+    const { secColor } = state.general;
+
+    function getCorrectBackground(color) {
+      if (color === 'rgba(0,0,0,0)') {
+        return `url(
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAABlBMVEXV1dXb29tFGkCIAAAAHklEQVR4AWNghAIGCMDgjwgFCDDSw2M0PSCD0fQAACRcAgF4ciGUAAAAAElFTkSuQmCC'
+      ) repeat`;
+      }
+      return color;
+    }
+
+    const primBackground = getCorrectBackground(primColor);
+    const secBackground = getCorrectBackground(secColor);
+
+    this.primColor.style.background = primBackground;
+    this.secColor.style.background = secBackground;
+  }
+
 }
