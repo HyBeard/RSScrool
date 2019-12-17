6744cd2a6ca315c70bee26e907f8738357ed3231
HyBeard <Kaptsevich.i@gmail.com>
2019-12-17 16:44:34 +0300

    feat: add functions to get day details

--- fancy-weather/src/js/helpers/common.js
+++ fancy-weather/src/js/helpers/common.js
@@ -17,3 +17,40 @@ export function getLocalTimeProps(momentObj = moment()) {
 export function getCountryTimeProps({ timezone }) {
   return getLocalTimeProps(moment.tz(timezone));
 }
+
+function getTimeOfYear({ monthNum }) {
+  if (monthNum <= 1 || monthNum === 11) {
+    return 'Winter';
+  }
+  if (monthNum <= 4) {
+    return 'Spring';
+  }
+  if (monthNum <= 7) {
+    return 'Summer';
+  }
+
+  return 'Autumn';
+}
+
+function getTimeOfDay(hour) {
+  return hour < 6 || hour > 21 ? 'night' : 'day';
+}
+
+export function getDayDetails({
+  monthNum, dayNum, hour, currentLang, glossary,
+}) {
+  const timeOfYear = getTimeOfYear(monthNum);
+  const month = glossary.month[currentLang][monthNum];
+  const dayOfWeek = glossary.dayOfWeek[currentLang][dayNum];
+  const dayOfWeekShort = glossary.dayOfWeekShort[currentLang][dayNum];
+  const timeOfDay = getTimeOfDay(hour);
+
+  return {
+    timeOfYear,
+    month,
+    dayNum,
+    dayOfWeek,
+    dayOfWeekShort,
+    timeOfDay,
+  };
+}
