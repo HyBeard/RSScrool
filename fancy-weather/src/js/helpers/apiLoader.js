81e2df045836100cb8afc2c80750edc429d09227
HyBeard <Kaptsevich.i@gmail.com>
2019-12-25 20:15:22 +0300

    fix: rework translating city and weather

--- fancy-weather/src/js/helpers/apiLoader.js
+++ fancy-weather/src/js/helpers/apiLoader.js
@@ -13,7 +13,7 @@ const apiLoader = {
   },
 
   async getImageUrl(query) {
-    const url = `https://api.unsplash.com/photos/random?query=${query}&client_id=ae8d7de6acc767654ce496c4d3cc5e08da2f74bae163d354d682e400d8cee33e`;
+    const url = `https://api.unsplash.com/photos/random?sig=${Math.random().toFixed(3)}&query=${query}&client_id=9a2a3a79ec7c1ff612c4f80e49d689119d1957beb8df77d0a20403f26dedaa1c`;
     const trustedUrl = this.passThroughProxy(url);
     const response = await this.getJson(trustedUrl);
 
@@ -29,8 +29,8 @@ const apiLoader = {
     return city;
   },
 
-  async getCityLocationInfo(city) {
-    const url = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=9dc8f5a4e2724c3aa83e3d0d471d2ad4&language=en&pretty=1&no_annotations=1&limit=1`;
+  async getCityLocationInfo(city, lang) {
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=9dc8f5a4e2724c3aa83e3d0d471d2ad4&language=${lang}&pretty=1&no_annotations=1&limit=1`;
 
     const response = await this.getJson(url);
     const {
@@ -41,17 +41,18 @@ const apiLoader = {
       cityFormatted: responseResult.formatted,
       latitude: responseResult.geometry.lat.toFixed(2),
       longitude: responseResult.geometry.lng.toFixed(2),
+      lang,
     };
   },
 
-  async getCity(latitude, longitude) {
-    const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C%20${longitude}&key=9dc8f5a4e2724c3aa83e3d0d471d2ad4&language=en&pretty=1&no_annotations=1&limit=1`;
+  async getCity({ latitude, longitude, lang }) {
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C%20${longitude}&key=9dc8f5a4e2724c3aa83e3d0d471d2ad4&language=${lang}&pretty=1&no_annotations=1&limit=1`;
 
     return this.getJson(url);
   },
 
-  async getWeather({ latitude, longitude }) {
-    const url = `https://api.darksky.net/forecast/3b0029e877b4499d720e83528314bf14/${latitude},${longitude}?units=si&lang=en&exclude=hourly,flags,alerts`;
+  async getWeather({ latitude, longitude, lang }) {
+    const url = `https://api.darksky.net/forecast/3b0029e877b4499d720e83528314bf14/${latitude},${longitude}?units=si&lang=${lang}&exclude=hourly,flags,alerts`;
     const trustedUrl = this.passThroughProxy(url);
     const response = await this.getJson(trustedUrl);
 
@@ -85,12 +86,14 @@ const apiLoader = {
     };
   },
 
-  async getTranslate(text, currentLang, lang) {
-    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191221T102000Z.c245c54bdeb89ff3.d5a49e3c6e952118a8401b3c1d7e18bd4a2ba856&text=${text}&lang=${currentLang}-${lang}`;
+  async getTranslate(textStrings, lang) {
+    const queries = textStrings.reduce((sumStr, str) => `${sumStr}&text=${str}`, '');
+
+    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191221T102000Z.c245c54bdeb89ff3.d5a49e3c6e952118a8401b3c1d7e18bd4a2ba856&${queries}&lang=${lang}`;
 
     const response = await this.getJson(url);
 
-    return response.text[0];
+    return response.text;
   },
 };
 
